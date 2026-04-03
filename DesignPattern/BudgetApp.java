ecd89f1ea3c0f3f1dc711adbb36d0bf58af7f7a5
Maddy400 <40018295@petroc.ac.uk>
2026-04-03 15:20:51 +0100

    Create test code

--- DesignPattern/BudgetApp.java
+++ DesignPattern/BudgetApp.java
@@ -2,28 +2,36 @@ public class BudgetApp{
 
     public static void main(String[] args){
 
-        //Creates a basic expense of 70
-        Expense expense1 = new BasicExpense(70);
+        //T-001
+        Expense test1 = new BasicExpense(70);
+        System.out.printf("T1: %s = £%.2f\n", test1.getDescription(), test1.getCost());
 
-        //Prints base expense
-        System.out.println(expense1.getDescription() + "= £" + expense1.getCost());
+        //T-002
+        Expense test2 = new TaxDecorator(new BasicExpense(70));
+        System.out.printf("T2: %s = £%.2f\n", test2.getDescription(), test2.getCost());
 
-        //Expense with tax applied
-        Expense expense2 = new TaxDecorator(new BasicExpense(70));
+        //T-003
+        Expense test3 = new ServiceFeeDecorator(new BasicExpense(70));
+        System.out.printf("T3: %s = £%.2f\n", test3.getDescription(), test3.getCost());
 
-        //Prints expense with tax
-        System.out.println(expense2.getDescription() + " = £" + expense2.getCost());
+        //T-004
+        Expense test4 = new DiscountDecorator(new BasicExpense(70));
+        System.out.printf("T4: %s = £%.2f\n", test4.getDescription(), test4.getCost());
 
-        //Expense with service fee, tax and discount
-        Expense expense3 = new DiscountDecorator(
-            new TaxDecorator(
-                new ServiceFeeDecorator(
-                    new BasicExpense(70)
-                )
-            )
-         );
+        //T-005
+        Expense test5 = new TaxDecorator(new ServiceFeeDecorator(new BasicExpense(70)));
+        System.out.printf("T5: %s = £%.2f\n", test5.getDescription(), test5.getCost());
 
-        //Print the fully decorated expense
-         System.out.println(expense3.getDescription() + " = £" + expense3.getCost());
+        //T-006
+        Expense test6 = new DiscountDecorator(new TaxDecorator(new ServiceFeeDecorator(new BasicExpense(70))));
+        System.out.printf("T6: %s = £%.2f\n", test6.getDescription(), test6.getCost());
+
+        //T-007
+        Expense test7 = new BasicExpense(0);
+        System.out.printf("T7: %s = £%.2f\n", test7.getDescription(), test7.getCost());
+
+        //T-008
+        Expense test8 = new BasicExpense(-10);
+        System.out.printf("T8: %s = £%.2f\n", test8.getDescription(), test8.getCost());
     }
 }
